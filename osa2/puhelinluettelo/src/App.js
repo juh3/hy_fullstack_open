cb8160ac8d4e2596a8b0c9359eec9f4a95ef984c
Juh3 <juha.tapio20@gmail.com>
2022-03-21 17:03:48 +0200

    Added the ability to add a phone number

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,14 +3,22 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '045-1231122'
+      }
   ]) 
   const [newName, setNewName] = useState('')
+  const[newNumber, setNewNumber] = useState('')
 
 
   const handleNameChange = (event) =>{
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) =>{
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   const addName=(event) =>{
@@ -25,10 +33,13 @@ const App = () => {
     // creating a nameObject with the newName
     const nameObject = {
       name: newName,
+      number: newNumber,
     }
     setPersons([...persons,nameObject])
     //reset the value of NewName, ready for a new name
     setNewName('')
+    console.log(persons)
+    
   }
   }
 
@@ -42,15 +53,24 @@ const App = () => {
           onChange = {handleNameChange}
           />
         </div>
+
+        <div>
+          number: <input
+          value = {newNumber}
+          onChange = {handleNumberChange}
+          />
+        </div>
+
         <div>
           <button type="submit">add</button>
         </div>
+
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person => (
           <p key = {person.name}>
-              {person.name}
+              {person.name} {person.number}
           </p>
         ))}
       </ul>
