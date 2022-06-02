3f6c31269a66883689dbabbf7c4ca7972f4f18a2
Juh3 <juha.tapio20@gmail.com>
2022-06-02 10:58:57 +0300

    Querys work, including parametric allBooks with author

--- osa8/library/index.js
+++ osa8/library/index.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose')
 const Book = require('./models/Book')
 const Author = require('./models/Author')
 
-const MONGODB_URI ='mongodb+srv://juh3:Natsumongo123@osa8.6aeay.mongodb.net/BookDatabase?retryWrites=true&w=majority'
-mongoose.connect(MONGODB_URI)
+const MONGODB_URI =
+  'mongodb+srv://juh3:Natsumongo123@osa8.6aeay.mongodb.net/BookDatabase?retryWrites=true&w=majority'
+mongoose
+  .connect(MONGODB_URI)
   .then(() => {
     console.log('connected to MongoDB')
   })
@@ -48,28 +50,40 @@ const typeDefs = gql`
 `
 const resolvers = {
   Query: {
-    bookCount: async() => Book.collection.countDocuments(),
-    authorCount: async() => Author.collection.countDocuments(),
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
 
-    allBooks: async(root, args) => {
-      return await Book.find({})      
+    allBooks: async (root, args) => {
+      if (args.author) {
+        const wantedAuthor = await Author.findOne({
+          name: args.author,
+        })
+        console.log(wantedAuthor)
+        if (wantedAuthor) {
+          console.log('im here')
+          if (args.genre && args.author) {
+            return (
+              (await Book.find({ genres: { $in: [args.genre] } })) &&
+              (await Book.find({
+                author: { $in: [wantedAuthor.id] },
+              }))
+            )
+          } else {
+            return await Book.find({
+              author: { $in: [wantedAuthor.id] },
+            })
+          }
+        }
+
+        if (!args.genre) {
+          return await Book.find({})
+        } else {
+          return Book.find({ genres: { $in: [args.genre] } })
+        }
+      }
     },
 
-      /*if (args.genre && args.author) {
-        return (
-          Book.find({genre: { $exists: args.genre === }})
-        &&
-          Book.find({ author: { $exists: args.author === author}})
-        )
-      }
-      if (args.genre && !args.author) {
-
-        return books.filter((b) => b.genres.includes(args.genre))
-      } else {
-        return books.filter((b) => b.author === args.author)
-      }*/
-  
-    allAuthors: async(root, args) => {
+    allAuthors: async (root, args) => {
       return Author.find({})
     },
   },
@@ -81,7 +95,10 @@ const resolvers = {
 
   Mutation: {
     addBook: async (root, args) => {
-      const authorObject = new Author({name: args.author, born: null})
+      const authorObject = new Author({
+        name: args.author,
+        born: null,
+      })
       /*if (authors.includes(args.author)) {
         books = books.concat(bookObject)
         return bookObject
@@ -91,21 +108,19 @@ const resolvers = {
           born: null,
           id: uuid(),
         }*/
-        await authorObject.save()
-        const newAuthor = await Author.findOne({ name: args.author })
-        const bookObject = new Book({...args, author: newAuthor})
+      await authorObject.save()
+      const newAuthor = await Author.findOne({ name: args.author })
+      const bookObject = new Book({ ...args, author: newAuthor })
 
-        return bookObject.save()
+      return bookObject.save()
+    },
 
-        
-      },
-
-    editAuthor: async(root, args) => {
-      const author = await Author.findOne({name: args.name})
+    editAuthor: async (root, args) => {
+      const author = await Author.findOne({ name: args.name })
       author.born = args.setBornTo
       return author.save()
     },
-}
+  },
 }
 
 const server = new ApolloServer({
